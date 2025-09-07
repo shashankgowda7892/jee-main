0b07b6848e7a78d7568f20ba30e699c42f2b61d8
Shashank Gowda C <136600133+shashankgowda7892@users.noreply.github.com>
2025-09-07 15:24:04 +0530

    feat: enhance getExams to support pagination and search functionality

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,6 @@
 const { Readable } = require("stream");
 const csv = require("csv-parser");
+const { Op } = require('sequelize');
 const { User,Question,Exam } = require('../models');
 
 const getUsers = async (req, res) => {
@@ -77,7 +78,24 @@ const uploadQuestions = async (req, res) => {
 
 const getExams = async (req, res) => {
   try {
-    const exams = await Exam.findAll();
+
+    const {search} = req.query;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
+    const exams = await Exam.findAll({
+      offset,
+      limit,
+      order: [
+      ['examDate', 'DESC']
+      ],
+      where:search ? {
+        examCode: {
+          [Op.like]: `%${search}%`
+        }
+      } : {}
+    });
     res.status(200).json({
       success: true,
       data: exams
